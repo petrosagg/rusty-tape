7e5840d4fb98065cc40bbd8c8deb15ad8673cbf4
Petros Angelatos <petrosagg@gmail.com>
2020-02-02 14:07:03 -0800

    store cassettes based on year/month to avoid duplicate names
    apparently there are many cassettes with the same name
    
    this commit also adds:
    
    * Sanitize cassettes names before writing the files
    * Simplify resume logic at the expense of a few more requests. Might
    revisit in the future
    
    Signed-off-by: Petros Angelatos <petrosagg@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,28 @@
 use std::process::Command;
 use std::os::unix::fs::symlink;
 use std::fs::{self, File};
+use std::path::Path;
 use std::io::prelude::*;
-use std::iter::FromIterator;
-use std::collections::{HashSet, HashMap};
+use std::collections::HashMap;
 use serde_json::Value;
 use scraper::{Html, Selector};
 use percent_encoding::percent_decode_str;
 
 #[derive(Debug)]
-struct Cassette {
+struct Cassette<'a> {
     name: String,
+    path: String,
     url: String,
     yt_url: String,
     labels: Vec<String>,
+    subcategories: Vec<&'a Subcategory>,
     created_at: String,
-}
-
-#[derive(Debug)]
-struct Category {
-    name: String,
-    subcategories: Vec<Subcategory>,
 }
 
 #[derive(Debug)]
 struct Subcategory {
     name: String,
-    parent: String,
+    category: String,
     kind: SubcategoryKind,
 }
 
@@ -42,7 +38,7 @@ async fn main() -> Result<(), reqwest::Error> {
     let selector = Selector::parse("ul#nav2 li a").unwrap();
     let content = Html::parse_document(&body);
 
-    let mut subcategories = vec![];
+    let mut all_subcategories = vec![];
 
     let mut categories = HashMap::new();
     for element in content.select(&selector) {
@@ -83,27 +79,19 @@ async fn main() -> Result<(), reqwest::Error> {
 
             let subcategory = Subcategory{
                 name: name,
-                parent: category.clone(),
+                category: category.clone(),
                 kind: kind,
             };
 
-            subcategories.push(subcategory);
+            all_subcategories.push(subcategory);
         }
     }
-
-    // populate the downloaded cassettes from previous runs
-    fs::create_dir_all("./cassettes").unwrap();
-    let entries = fs::read_dir("./cassettes/")
-        .unwrap()
-        .map(|res| res.unwrap().file_name().into_string().unwrap());
-    let downloaded_cassettes: HashSet<String> = HashSet::from_iter(entries);
 
     let mut index = 1;
     let mut cassettes = Vec::new();
 
     let selector = Selector::parse("iframe").unwrap();
-    // Start gathering all cassettes until we hit the first one that is already downloaded
-    'outer: loop {
+    loop {
         let url = format!("https://www.kasetophono.com/feeds/posts/default?alt=json&start-index={}", index);
         let body = reqwest::get(&url).await?.text().await?;
 
@@ -119,26 +107,39 @@ async fn main() -> Result<(), reqwest::Error> {
 
                     if let Some(yt_url) = url.filter(|u| u.contains("videoseries")) {
                         let name = entry["title"]["$t"].as_str().unwrap().trim();
-                        if downloaded_cassettes.contains(name) {
-                            println!("found already downloaded cassette {}", name);
-                            break 'outer;
-                        }
+                        let url = entry["link"][2]["href"].as_str().unwrap().to_string();
+
+                        // Extract year and date from URL like this: https://www.kasetophono.com/2019/01/nero.html
+                        let mut path: Vec<&str> = url.split('/').rev().skip(1).take(2).chain(Some("cassettes")).collect();
+                        path.reverse();
+                        // Some cassettes contain slashes in their names
+                        let safe_name = name.replace('/', "-");
+                        path.push(&safe_name);
+                        let path = path.join("/");
+
+                        let published = entry["published"]["$t"].as_str().unwrap();
 
                         let labels = entry["category"]
                             .as_array()
                             .map(|cats| {
                                 cats.iter()
                                     .filter_map(|c| c["term"].as_str())
+                                    .map(|s| s.to_string())
                                     .collect()
                             }).unwrap_or(vec![]);
-                        
-                        let published = entry["published"]["$t"].as_str().unwrap();
 
-                        let url = entry["link"][2]["href"].as_str().unwrap().to_string();
+                        let subcategories = all_subcategories.iter().filter(|sc| {
+                            match &sc.kind {
+                                SubcategoryKind::Label(l) => labels.contains(l),
+                                SubcategoryKind::Cassette(u) => url == *u,
+                            }
+                        }).collect();
 
                         let cassette = Cassette{
                             name: name.to_string(),
-                            labels: labels.into_iter().map(String::from).collect(),
+                            path: path,
+                            subcategories: subcategories,
+                            labels: labels,
                             url: url,
                             yt_url: yt_url.to_string(),
                             created_at: published.to_string(),
@@ -157,6 +158,10 @@ async fn main() -> Result<(), reqwest::Error> {
     fs::remove_dir_all(".tmp-cassette").unwrap_or(());
 
     for cassette in cassettes.iter().rev() {
+        if Path::new(&cassette.path).exists() {
+            println!("Skipping already downloaded cassette {}", cassette.name);
+            continue;
+        }
         fs::create_dir(".tmp-cassette").unwrap();
 
         Command::new("youtube-dl")
@@ -191,28 +196,25 @@ async fn main() -> Result<(), reqwest::Error> {
         let mut file = File::create(".tmp-cassette/playlist.m3u").unwrap();
         file.write_all(&buf).unwrap();
 
-        let path = format!("cassettes/{}", cassette.name);
-        fs::rename(".tmp-cassette", &path).unwrap();
+        fs::create_dir_all(Path::new(&cassette.path).parent().unwrap()).unwrap();
+        fs::rename(".tmp-cassette", &cassette.path).unwrap();
 
         for label in &cassette.labels {
-            // First write the playlsit in the tag directory
             let label_path = format!("labels/{}", label);
             fs::create_dir_all(&label_path).unwrap();
-            symlink(format!("../../{}", path), format!("{}/{}", &label_path, cassette.name)).unwrap();
+            let src = format!("../../{}", cassette.path);
+            let dest = format!("{}/{}", &label_path, cassette.name);
+            println!("Symlinking {} -> {}", dest, src);
+            symlink(src, dest).unwrap();
+        }
 
-            // Then add cassette to subcategories that are of this label or include the cassette
-            let subcategories = subcategories.iter().filter(|sc| {
-                match &sc.kind {
-                    SubcategoryKind::Label(l) => *label == *l,
-                    SubcategoryKind::Cassette(u) => cassette.url == *u,
-                }
-            });
-
-            for subcategory in subcategories {
-                let subcategory_path = format!("categories/{}/{}", subcategory.parent, subcategory.name);
-                fs::create_dir_all(&subcategory_path).unwrap();
-                symlink(format!("../../../{}", path), format!("{}/{}", &subcategory_path, cassette.name)).unwrap();
-            }
+        for subcategory in &cassette.subcategories {
+            let subcategory_path = format!("categories/{}/{}", subcategory.category, subcategory.name);
+            fs::create_dir_all(&subcategory_path).unwrap();
+            let src = format!("../../../{}", cassette.path);
+            let dest = format!("{}/{}", &subcategory_path, cassette.name);
+            println!("Symlinking {} -> {}", dest, src);
+            symlink(src, dest).unwrap();
         }
     }
 
