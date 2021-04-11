68fdefa3048accf4ace978a07f3199e6c56c280f
Petros Angelatos <petrosagg@gmail.com>
2021-04-11 18:45:27 +0200

    fetch metadata in parallel and offer a basic HTTP API
    Signed-off-by: Petros Angelatos <petrosagg@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,25 @@ use std::os::unix::fs::symlink;
 use std::fs::{self, File};
 use std::path::Path;
 use std::io::prelude::*;
+use std::sync::Arc;
 use std::collections::HashMap;
-use serde::Serialize;
+use serde::{Serialize, Deserialize};
 use serde_json::Value;
 use scraper::{Html, Selector};
 use percent_encoding::percent_decode_str;
 use rayon::prelude::*;
+use futures::stream::{Stream, StreamExt, TryStreamExt};
+use std::future;
+use warp::Filter;
+use uuid::Uuid;
+use std::sync::Mutex;
+use std::process::Child;
 
 mod audio;
 
-#[derive(Debug,Serialize)]
-pub struct Cassette<'a> {
+#[derive(Debug,Serialize,Deserialize)]
+pub struct Cassette {
+    pub uuid: Uuid,
     pub name: String,
     pub safe_name: String,
     pub path: String,
@@ -21,25 +29,24 @@ pub struct Cassette<'a> {
     pub yt_url: String,
     pub image_url: Option<String>,
     pub labels: Vec<String>,
-    pub subcategories: Vec<&'a Subcategory>,
+    pub subcategories: Vec<Subcategory>,
     pub created_at: String,
 }
 
-#[derive(Debug,Serialize)]
+#[derive(Clone,Debug,Serialize,Deserialize)]
 pub struct Subcategory {
     name: String,
     category: String,
     kind: SubcategoryKind,
 }
 
-#[derive(Debug,Serialize)]
+#[derive(Clone,Debug,Serialize,Deserialize)]
 enum SubcategoryKind {
     Label(String),
     Cassette(String),
 }
 
-#[tokio::main]
-async fn main() -> Result<(), reqwest::Error> {
+async fn categories() -> Result<Vec<(String, String)>, anyhow::Error> {
     let body = reqwest::get("https://kasetophono.com").await?.text().await?;
     let content = Html::parse_document(&body);
 
@@ -62,189 +69,268 @@ async fn main() -> Result<(), reqwest::Error> {
                 })
                 .collect::<String>();
 
-            categories.entry(href).or_insert(name);
+            categories.entry(href.to_owned()).or_insert(name);
         }
     }
 
-    let mut all_subcategories = vec![];
+    Ok(categories.drain().collect())
+}
 
-    for (url, category) in &mut categories {
-        let body = reqwest::get(*url).await?.text().await?;
-        let content = Html::parse_document(&body);
+async fn subcategories(categories: &[(String, String)]) -> Result<Vec<Subcategory>, anyhow::Error> {
+    futures::stream::iter(categories)
+        .map(|(url, category)| async move {
+            let body = reqwest::get(url).await?.text().await?;
+            println!("get: {}", url);
+            let content = Html::parse_document(&body);
 
-        let selector = Selector::parse("div.post-body h1.favourite-posts-title a").unwrap();
+            let selector = Selector::parse("div.post-body h1.favourite-posts-title a").unwrap();
 
-        for element in content.select(&selector) {
-            let name = element.text().next().unwrap().trim().to_string();
-            let href = element.value().attr("href").unwrap();
+            let mut subcategories = vec![];
 
-            let kind = if href.contains("/label/") {
-                let label_raw = href.split_at(href.rfind('/').unwrap() + 1).1;
-                SubcategoryKind::Label(percent_decode_str(label_raw).decode_utf8().unwrap().into_owned())
-            } else {
-                SubcategoryKind::Cassette(href.to_string())
-            };
+            for element in content.select(&selector) {
+                let name = element.text().next().unwrap().trim().to_string();
+                let href = element.value().attr("href").unwrap();
 
-            let subcategory = Subcategory{
-                name: name,
-                category: category.clone(),
-                kind: kind,
-            };
+                let kind = if href.contains("/label/") {
+                    let label_raw = href.split_at(href.rfind('/').unwrap() + 1).1;
+                    SubcategoryKind::Label(percent_decode_str(label_raw).decode_utf8().unwrap().into_owned())
+                } else {
+                    SubcategoryKind::Cassette(href.to_string())
+                };
 
-            all_subcategories.push(subcategory);
-        }
-    }
+                let subcategory = Subcategory{
+                    name: name,
+                    category: category.clone(),
+                    kind: kind,
+                };
+                subcategories.push(subcategory);
+            }
 
-    let mut index = 1;
-    let mut cassettes = Vec::new();
+            Ok(subcategories)
+        })
+        .buffer_unordered(5)
+        .try_fold(vec![], |mut v, s| {
+            v.extend(s);
+            future::ready(Ok(v))
+        }).await
+}
+
+async fn cassette_range(subcategories: &[Subcategory], offset: u64, len: u64) -> Result<Vec<(Uuid, Cassette)>, anyhow::Error> {
+    assert!(len <= 25);
+    let mut cassettes = vec![];
 
     let iframe_selector = Selector::parse("iframe").unwrap();
     let image_selector = Selector::parse("img").unwrap();
-    loop {
-        let url = format!("https://www.kasetophono.com/feeds/posts/default?alt=json&start-index={}", index);
-        let body = reqwest::get(&url).await?.text().await?;
 
-        let v: Value = serde_json::from_str(&body).unwrap();
+    let url = format!("https://www.kasetophono.com/feeds/posts/default?alt=json&start-index={}&max-results={}", offset + 1, len);
+    println!("get: {}", url);
+    let body = reqwest::get(&url).await?.text().await?;
 
-        match v["feed"]["entry"].as_array() {
-            Some(entries) => {
-                index += entries.len();
+    let v: Value = serde_json::from_str(&body).unwrap();
 
-                for entry in entries {
-                    let content = Html::parse_fragment(entry["content"]["$t"].as_str().unwrap());
-                    let url = content.select(&iframe_selector).next().and_then(|e| e.value().attr("src"));
+    if let Some(entries) = v["feed"]["entry"].as_array() {
+        for entry in entries {
+            let content = Html::parse_fragment(entry["content"]["$t"].as_str().unwrap());
+            let url = content.select(&iframe_selector).next().and_then(|e| e.value().attr("src"));
 
-                    if let Some(yt_url) = url.filter(|u| u.contains("videoseries")) {
-                        let name = entry["title"]["$t"].as_str().unwrap().trim();
-                        // Some cassettes contain slashes in their names
-                        let safe_name = name.replace('/', "-");
-                        let url = entry["link"][2]["href"].as_str().unwrap().to_string();
+            if let Some(yt_url) = url.filter(|u| u.contains("youtube.com") && u.contains("list")) {
+                let name = entry["title"]["$t"].as_str().unwrap().trim();
+                // Some cassettes contain slashes in their names
+                let safe_name = name.replace('/', "-");
+                let url = entry["link"][2]["href"].as_str().unwrap().to_string();
 
-                        // Extract year and date from URL like this: https://www.kasetophono.com/2019/01/nero.html
-                        let mut path: Vec<&str> = url.split('/').rev().skip(1).take(2).chain(Some("cassettes")).collect();
-                        path.reverse();
-                        path.push(&safe_name);
-                        let path = path.join("/");
+                let uuid = Uuid::new_v5(&Uuid::NAMESPACE_URL, url.as_bytes());
 
-                        let published = entry["published"]["$t"].as_str().unwrap();
+                // Extract year and date from URL like this: https://www.kasetophono.com/2019/01/nero.html
+                let mut path: Vec<&str> = url.split('/').rev().skip(1).take(2).chain(Some("cassettes")).collect();
+                path.reverse();
+                path.push(&safe_name);
+                let path = path.join("/");
 
-                        let labels = entry["category"]
-                            .as_array()
-                            .map(|cats| {
-                                cats.iter()
-                                    .filter_map(|c| c["term"].as_str())
-                                    .map(|s| s.to_string())
-                                    .collect()
-                            }).unwrap_or(vec![]);
+                let published = entry["published"]["$t"].as_str().unwrap();
 
-                        let subcategories = all_subcategories.iter().filter(|sc| {
-                            match &sc.kind {
-                                SubcategoryKind::Label(l) => labels.contains(l),
-                                SubcategoryKind::Cassette(u) => url == *u,
-                            }
-                        }).collect();
+                let labels = entry["category"]
+                    .as_array()
+                    .map(|cats| {
+                        cats.iter()
+                            .filter_map(|c| c["term"].as_str())
+                            .map(|s| s.to_string())
+                            .collect()
+                    }).unwrap_or(vec![]);
 
-                        let image = content.select(&image_selector).next().and_then(|e| e.value().attr("src"));
-
-                        let cassette = Cassette{
-                            name: name.to_string(),
-                            safe_name: safe_name,
-                            path: path,
-                            subcategories: subcategories,
-                            labels: labels,
-                            image_url: image.map(|s| s.to_string()),
-                            url: url,
-                            yt_url: yt_url.to_string(),
-                            created_at: published.to_string(),
-                        };
-
-                        println!("{:#?}", cassette);
-                        cassettes.push(cassette);
+                let subcategories = subcategories.iter().cloned().filter(|sc| {
+                    match &sc.kind {
+                        SubcategoryKind::Label(l) => labels.contains(l),
+                        SubcategoryKind::Cassette(u) => url == *u,
                     }
-                }
+                }).collect();
+
+                let image = content.select(&image_selector).next().and_then(|e| e.value().attr("src"));
+
+                let cassette = Cassette{
+                    uuid,
+                    name: name.to_string(),
+                    safe_name: safe_name,
+                    path: path,
+                    subcategories: subcategories,
+                    labels: labels,
+                    image_url: image.map(|s| s.to_string()),
+                    url: url,
+                    yt_url: yt_url.to_string(),
+                    created_at: published.to_string(),
+                };
+
+                cassettes.push((uuid.clone(), cassette));
             }
-            None => break
         }
     }
+    Ok(cassettes)
+}
 
-    {
+fn cassettes(subcategories: &[Subcategory]) -> impl Stream<Item=Result<Vec<(Uuid, Cassette)>, anyhow::Error>> + '_ {
+    futures::stream::iter(0..)
+        .map(move |page| cassette_range(subcategories, page * 25, 25))
+        .buffer_unordered(5)
+        .try_take_while(|page| future::ready(Ok(!page.is_empty())))
+}
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    let cassettes: HashMap<Uuid, Cassette> = if let Ok(content) = std::fs::read_to_string("metadata.json") {
+        println!("Loading cassettes from disk");
+        serde_json::from_str(&content).unwrap()
+    } else {
+        println!("Loading cassettes from upstream");
+        let categories = categories().await?;
+
+        let subcategories = subcategories(&categories).await?;
+
+        let cassette_stream = cassettes(&subcategories);
+        tokio::pin!(cassette_stream);
+
+        let mut cassettes = HashMap::new();
+        while let Some(Ok(page)) = cassette_stream.next().await {
+            cassettes.extend(page);
+        }
+
         let buf = serde_json::to_string_pretty(&cassettes).unwrap();
         let mut file = File::create("metadata.json").unwrap();
         file.write_all(buf.as_bytes()).unwrap();
-    }
+        cassettes
+    };
 
-    // remove any partial downloads from previous runs
-    fs::remove_dir_all(".tmp-cassette").unwrap_or(());
+    let cassettes = Arc::new(cassettes);
+    let state: Arc<Mutex<Option<Child>>> = Arc::new(Mutex::new(None));
 
-    for cassette in cassettes.iter().rev() {
-        if Path::new(&cassette.path).exists() {
-            println!("Skipping already downloaded cassette {}", cassette.name);
-            continue;
-        }
-        fs::create_dir(".tmp-cassette").unwrap();
+    let play_state = Arc::clone(&state);
+    let play = warp::path!("play" / Uuid)
+        .map(move |uuid: Uuid| {
+            let cassette = &cassettes[&uuid];
 
-        let status = Command::new("youtube-dl")
-            .current_dir(".tmp-cassette")
-            .args(&[
-                "--extract-audio",
-                "--audio-format", "mp3",
-                "--audio-quality", "2",
-                "--add-metadata",
-                "--geo-bypass-country", "GR",
-                "--ignore-errors",
-                "--output", "%(playlist_index)s - %(title)s.%(ext)s",
-                &cassette.yt_url,
-            ])
-            .status()
-            .expect("failed to execute youtube-dl");
-
-        if !status.success() {
-            panic!();
-        }
-
-        if let Some(url) = &cassette.image_url {
-            let data = reqwest::get(url).await?.bytes().await?;
-            let mut thumb = File::create(".tmp-cassette/thumbnail.gif").unwrap();
-
-            thumb.write_all(data.as_ref()).unwrap();
-        }
-
-        let mut songs = fs::read_dir(".tmp-cassette")
-            .unwrap()
-            .map(|res| res.unwrap().path().to_str().unwrap().to_string())
-            .filter(|p| !p.contains("thumbnail.gif"))
-            .collect::<Vec<_>>();
-        songs.sort_unstable();
-
-        let total = songs.len() as u8;
-        songs.par_iter().enumerate().for_each(|(i, song)| {
-            println!("Normalizing track: {}", song);
-            let l = audio::measure_loudness(song);
-            audio::correct_loudness(song, song, l);
-            audio::add_cassette_metadata(song, song, &cassette, (i + 1) as u8, total, ".tmp-cassette/thumbnail.gif");
+            println!("playing {}", &cassette.name);
+            let mut state = play_state.lock().unwrap();
+            if let Some(mut handle) = state.take() {
+                handle.kill();
+            }
+            let handle = std::process::Command::new("mpv")
+                .args(&["--no-video", &cassette.yt_url])
+                .spawn()
+                .unwrap();
+            *state = Some(handle);
+            format!("{:?}", &cassette.name)
         });
 
-        fs::create_dir_all(Path::new(&cassette.path).parent().unwrap()).unwrap();
-        fs::rename(".tmp-cassette", &cassette.path).unwrap();
+    let stop_state = Arc::clone(&state);
+    let stop = warp::path!("stop")
+        .map(move || {
+            println!("stopping");
+            let mut state = stop_state.lock().unwrap();
+            if let Some(mut handle) = state.take() {
+                handle.kill();
+            }
+            format!("Killed")
+        });
 
-        for label in &cassette.labels {
-            let label_path = format!("labels/{}", label);
-            fs::create_dir_all(&label_path).unwrap();
-            let src = format!("../../{}", cassette.path);
-            let dest = format!("{}/{}", &label_path, cassette.safe_name);
-            println!("Symlinking {} -> {}", dest, src);
-            symlink(src, dest).unwrap();
-        }
+    let cassettes = warp::path!("cassettes")
+        .and(warp::fs::file("metadata.json"))
+        .with(warp::compression::gzip());
 
-        for subcategory in &cassette.subcategories {
-            let subcategory_path = format!("categories/{}/{}", subcategory.category, subcategory.name);
-            fs::create_dir_all(&subcategory_path).unwrap();
-            let src = format!("../../../{}", cassette.path);
-            let dest = format!("{}/{}", &subcategory_path, cassette.safe_name);
-            println!("Symlinking {} -> {}", dest, src);
-            symlink(src, dest).unwrap();
-        }
-    }
+    let routes = warp::get().and(play.or(stop).or(cassettes));
+
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+
+    // // remove any partial downloads from previous runs
+    // fs::remove_dir_all(".tmp-cassette").unwrap_or(());
+
+    // for cassette in cassettes.iter().rev() {
+    //     if Path::new(&cassette.path).exists() {
+    //         println!("Skipping already downloaded cassette {}", cassette.name);
+    //         continue;
+    //     }
+    //     fs::create_dir(".tmp-cassette").unwrap();
+
+    //     let status = Command::new("youtube-dl")
+    //         .current_dir(".tmp-cassette")
+    //         .args(&[
+    //             "--extract-audio",
+    //             "--audio-format", "mp3",
+    //             "--audio-quality", "2",
+    //             "--add-metadata",
+    //             "--geo-bypass-country", "GR",
+    //             "--ignore-errors",
+    //             "--output", "%(playlist_index)s - %(title)s.%(ext)s",
+    //             &cassette.yt_url,
+    //         ])
+    //         .status()
+    //         .expect("failed to execute youtube-dl");
+
+    //     if !status.success() {
+    //         panic!();
+    //     }
+
+    //     if let Some(url) = &cassette.image_url {
+    //         let data = reqwest::get(url).await?.bytes().await?;
+    //         let mut thumb = File::create(".tmp-cassette/thumbnail.gif").unwrap();
+
+    //         thumb.write_all(data.as_ref()).unwrap();
+    //     }
+
+    //     let mut songs = fs::read_dir(".tmp-cassette")
+    //         .unwrap()
+    //         .map(|res| res.unwrap().path().to_str().unwrap().to_string())
+    //         .filter(|p| !p.contains("thumbnail.gif"))
+    //         .collect::<Vec<_>>();
+    //     songs.sort_unstable();
+
+    //     let total = songs.len() as u8;
+    //     songs.par_iter().enumerate().for_each(|(i, song)| {
+    //         println!("Normalizing track: {}", song);
+    //         let l = audio::measure_loudness(song);
+    //         audio::correct_loudness(song, song, l);
+    //         audio::add_cassette_metadata(song, song, &cassette, (i + 1) as u8, total, ".tmp-cassette/thumbnail.gif");
+    //     });
+
+    //     fs::create_dir_all(Path::new(&cassette.path).parent().unwrap()).unwrap();
+    //     fs::rename(".tmp-cassette", &cassette.path).unwrap();
+
+    //     for label in &cassette.labels {
+    //         let label_path = format!("labels/{}", label);
+    //         fs::create_dir_all(&label_path).unwrap();
+    //         let src = format!("../../{}", cassette.path);
+    //         let dest = format!("{}/{}", &label_path, cassette.safe_name);
+    //         println!("Symlinking {} -> {}", dest, src);
+    //         symlink(src, dest).unwrap();
+    //     }
+
+    //     for subcategory in &cassette.subcategories {
+    //         let subcategory_path = format!("categories/{}/{}", subcategory.category, subcategory.name);
+    //         fs::create_dir_all(&subcategory_path).unwrap();
+    //         let src = format!("../../../{}", cassette.path);
+    //         let dest = format!("{}/{}", &subcategory_path, cassette.safe_name);
+    //         println!("Symlinking {} -> {}", dest, src);
+    //         symlink(src, dest).unwrap();
+    //     }
+    // }
 
     Ok(())
 }
