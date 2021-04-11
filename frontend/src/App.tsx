f34bf6377473cfe0bf7fcd2931b28dadb4532282
Petros Angelatos <petrosagg@gmail.com>
2021-04-11 21:02:13 +0200

    use a table and display creation time
    Signed-off-by: Petros Angelatos <petrosagg@gmail.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { Button, List } from "semantic-ui-react";
+import { Button, Table } from "semantic-ui-react";
+import moment from "moment";
 
 type Cassette = {
   uuid: string;
   name: string;
+  url: string;
+  created_at: string;
 };
 
 export default function App() {
@@ -40,15 +43,27 @@ export default function App() {
   } else {
     return (
       <div>
-        <Button onClick={stop}>Stop</Button>
-        <List>
-          {Object.values(items).map((item) => (
-            <List.Item key={item.uuid}>
-              {item.name}
-              <Button onClick={() => play(item.uuid)}>Play</Button>
-            </List.Item>
-          ))}
-        </List>
+        <Button color="red" onClick={stop}>Stop</Button>
+        <Table celled sortable>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell />
+              <Table.HeaderCell>Name</Table.HeaderCell>
+              <Table.HeaderCell>Created At</Table.HeaderCell>
+              <Table.HeaderCell>Link</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {Object.values(items).sort((a, b) => a.created_at < b.created_at ? 1 : -1).map((item) => (
+              <Table.Row key={item.uuid}>
+                <Table.Cell><Button color="green" onClick={() => play(item.uuid)}>Play</Button></Table.Cell>
+                <Table.Cell>{item.name}</Table.Cell>
+                <Table.Cell>{moment(item.created_at).format("MMMM YYYY")}</Table.Cell>
+                <Table.Cell><a href={item.url}>Link</a></Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table>
       </div>
     );
   }
