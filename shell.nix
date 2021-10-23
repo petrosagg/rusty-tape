b350400a0ddbb8fc7525cf2b11531ec85f08e9c9
Petros Angelatos <petrosagg@gmail.com>
2021-10-24 00:51:31 +0200

    update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,15 +1,27 @@
 with import <nixpkgs> {};
 
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
     name = "rusty-tape";
     buildInputs = [
         openssl
         pkgconfig
         youtube-dl
+        freetype
+        expat
+        libxml2
+        libxkbcommon
         ffmpeg
-        cargo
         rustup
         nodejs
         mpv
+        vulkan-loader
+        vulkan-tools
+        xorg.libX11
+        xorg.libXcursor
+        xorg.libXrandr
+        xorg.libXi
     ];
+    shellHook = ''
+        export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${lib.makeLibraryPath buildInputs}";
+    '';
 }
