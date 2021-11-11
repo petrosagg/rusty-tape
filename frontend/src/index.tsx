a395777c92576f7783ab87f1ddf6f3b4aca7f7d8
Petros Angelatos <petrosagg@gmail.com>
2021-11-11 12:13:30 +0100

    remove of javascript based ui
    Signed-off-by: Petros Angelatos <petrosagg@gmail.com>

--- frontend/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "semantic-ui-css/semantic.min.css";
-
-const root = document.createElement("div");
-document.body.appendChild(root);
-
-ReactDOM.render(<App />, root);
